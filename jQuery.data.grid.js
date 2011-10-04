d8f61a92c93f9c61283b7b4582e6a44861ebaca7
Ian Hoar <ian.hoar@imhmedia.net>
2011-10-04 19:23:00 -0400

    New snippet data grid

---
+++ jQuery.data.grid.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	
+	// set up data grid 
+	$('.data-grid tr:odd').addClass('odd');
+	$('.data-grid td:last-child, .data-grid th:last-child').css({'border-right' : 'none'});
+
+	colNarrow = $('.col-narrow').width()-2; // - 1px borders x2
+	colWide = $('.col-wide').width()-2; // - 1px borders x2
+	colFull = $('.col-full').width()-2;
+	
+	$('.data-grid').each(function() {
+		if($(this).parents().hasClass('col-wide')) {
+			$(this).width(colWide);
+		} else if($(this).parents().hasClass('col-narrow')) {
+			$(this).width(colNarrow);
+		} else if($(this).parents().hasClass('col-full')) {
+			$(this).width(colFull)
+		}
+	});
+
+});
