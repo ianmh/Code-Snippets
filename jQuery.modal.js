c4e7dc37bae97d9223d1959ccdedd9446b81d232
Ian Hoar <ian.hoar@imhmedia.net>
2011-10-19 18:33:28 -0400

    updates

--- jQuery.modal.js
+++ jQuery.modal.js
@@ -2,8 +2,26 @@
 		// Get element value
 		var elem = elem;
 		$(elem).hide();
-		
-		// screen dementions and document height
+	
+		// center modal
+		if($(elem).height() > $(window).height()) {
+			$(elem).css("top", '20px');
+		} else {
+			$(elem).css("top", ($(window).height() - $(elem).height()) / 2 + $(window).scrollTop());
+		}
+		if($(elem).width() > $(window).width()) {
+			$(elem).css("left", '20px');
+		} else {
+			$(elem).css("left", ($(window).width() - $(elem).width()) / 2 + $(window).scrollLeft());
+		}
+		$(elem).css({
+			'position' : 'absolute',
+			'z-index' : '10001'
+		});
+
+		$(elem).fadeIn('fast');
+
+		// screen dimentions and document height
 		var winWidth = $(window).width();
 		var winHeight = $(window).height();
 		var docHeight = $(document).height();
@@ -17,44 +35,17 @@
 			'position': 'absolute',
 			'background': 'black',
 			'z-index': 10000,
-			'opacity':0
+			'opacity':0,
+			'padding-bottom': '20px'
 		});
-		// transition
+		// Transition
 		$('#mask').animate({'opacity':.5}, 200);
-	
-		// center modal
-		if($(elem).height() > $(window).height()) {
-			$(elem).css("top", 0);
-		} else {
-			$(elem).css("top", ($(window).height() - $(elem).height()) / 2 + $(window).scrollTop());
-		}
-		if($(elem).width() > $(window).width()) {
-			$(elem).css("left", 0);
-		} else {
-			$(elem).css("left", ($(window).width() - $(elem).width()) / 2 + $(window).scrollLeft());
-		}
-		$(elem).css({
-			'position' : 'absolute',
-			'z-index' : '10001'
-		});
 		
-		$(elem).fadeIn('fast');
-	
 		$(elem + ' .close' + ', #mask').click(function() {
 			$('#mask').fadeOut('fast');
 			$(elem).fadeOut('fast');
 		});
+	
 	}
 	
 	$('.modal').hide();
-
-
-
-	// Modal clicks
-	$('#save').click(function() {
-		openModal('.m-save');
-	});
-	$('#preview').click(function() {
-		openModal('.m-preview');
-	});
-	
